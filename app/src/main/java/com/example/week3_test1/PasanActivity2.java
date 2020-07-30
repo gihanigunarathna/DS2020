31bf6250a513a663960b934ed74eed72622528dc
gihanigunarathna <gihanigunarathna88@gmail.com>
2020-07-30 09:01:41 +0530

    Buwaneka branch commit

---
+++ app/src/main/java/com/example/week3_test1/PasanActivity2.java
@@ -0,0 +1,14 @@
+package com.example.week3_test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasanActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasan2);
+    }
+}
